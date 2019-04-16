24768813160c21350805e025ded3f8cfd1ff0f7b
vickyteng <vteng0406@gmail.com>
2019-04-16 19:30:30 -0400

    Added sign up and login storyboards

---
+++ TerpMarketplace/TerpMarketplace/SignUpViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SignUpViewController.swift
+//  TerpMarketplace
+//
+//  Created by Victoria Teng on 4/16/19.
+//  Copyright © 2019 CMSC436. All rights reserved.
+//
+
+import UIKit
+// import Firebase
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
